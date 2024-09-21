a8a434045159b2336ae5803d05921c391e09fd38
alexshagadev <alexshagabutdinov@gmail.com>
2024-09-21 14:31:38 -0600

    Finish week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>Alexandr Shagabutdinov</h1>
+            <Link href="https://github.com/alexshagadev">My GitHub</Link>
+        </div>
+    );
+}
